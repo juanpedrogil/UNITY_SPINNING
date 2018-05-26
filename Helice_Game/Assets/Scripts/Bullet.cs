2d74837007a4030b6f7c5e9c638da7d02b81f546
Juan Pedro Gil Llanos <juanpedrogilllanos@hotmail.com>
2018-05-25 23:25:04 -0600

    Disparo de barco

---
+++ Helice_Game/Assets/Scripts/Bullet.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+	public string direction;
+	public float speed;
+	private Rigidbody2D rb;
+	private float velX,velY;
+	// Use this for initialization
+	void Start () {
+		rb=GetComponent<Rigidbody2D>();
+		velX=rb.velocity.x;
+		velY=rb.velocity.y;
+		switch(direction){
+			case "up":moveUp();
+				break;
+			case "down":moveDown();
+				break;
+			case "left":moveLeft();
+				break;
+			case "right":moveRight();
+				break;
+		}
+	}
+	private void moveUp(){
+		rb.velocity=new Vector2(velX,velY+speed);
+	}
+	private void moveDown(){
+		rb.velocity=new Vector2(velX,velY-speed);
+	}
+	private void moveLeft(){
+		rb.velocity=new Vector2(velX-speed,velY);
+	}
+	private void moveRight(){
+		rb.velocity=new Vector2(velX+speed,velY);
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Helice_Game/Assets/Scripts/Shot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour {
+	public GameObject explosion,bullet;
+	private Vector3 position;
+	// Use this for initialization
+	void Start () {
+		position = GetComponent<Transform>().position;
+		shotTheBullet();
+	}
+	private void shotTheBullet(){
+		Instantiate(explosion,position,Quaternion.identity);
+		Instantiate(bullet,position,Quaternion.identity);
+		Invoke("shotTheBullet",Random.Range(1.5f,3f));
+	}
+}
